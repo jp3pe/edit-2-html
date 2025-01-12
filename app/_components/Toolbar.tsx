21be90db4551df3c23e10d0ef5fb7af76bd6e4d1
jp3pe <rlaworhkd430430@gmail.com>
2025-01-12 18:35:04 +0900

    Align the elements inside the toolbar horizontally in the center.

--- app/_components/Toolbar.tsx
+++ app/_components/Toolbar.tsx
@@ -15,39 +15,49 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import InsertFontIcon from "@/public/icons/font-color-icon.svg";
 
+import styles from "@/app/styles.module.css";
+
 interface ToolbarProps {}
 
 export default function Toolbar({}: ToolbarProps) {
   return (
-    <div>
-      <div>
-        <a>
+    <div className={styles.toolbar}>
+      {/* TODO: Replace a tag into next Link */}
+      <a href="#">
+        <div className={styles["toolbar-info"]}>
           {/* TODO: 물음표 기호의 배경과 아이콘을 반전하는 방법 찾기 */}
           <FontAwesomeIcon
             icon={faCircleQuestion}
             style={{ color: "var(--arrow-color)" }}
           />
           <p>Info</p>
-        </a>
-      </div>
+        </div>
+      </a>
       {/* TODO: palets horizon line */}
-      <div>
-        <p>Text</p>
-        <FontAwesomeIcon icon={faAngleDown} style={{ color: "var(--arrow-color)" }} />
-      </div>
+      <p>Text</p>
+      <FontAwesomeIcon
+        icon={faAngleDown}
+        style={{ color: "var(--arrow-color)" }}
+      />
       <FontAwesomeIcon icon={faBold} />
       <FontAwesomeIcon icon={faItalic} />
       <FontAwesomeIcon icon={faUnderline} />
       <FontAwesomeIcon icon={faStrikethrough} />
       <FontAwesomeIcon icon={faCode} />
       <FontAwesomeIcon icon={faSquareRootVariable} />
-      <div>
+      <div className={styles["toolbar-link"]}>
         <FontAwesomeIcon icon={faLink} />
-        <FontAwesomeIcon icon={faAngleDown} style={{ color: "var(--arrow-color)" }} />
+        <FontAwesomeIcon
+          icon={faAngleDown}
+          style={{ color: "var(--arrow-color)" }}
+        />
       </div>
-      <div>
+      <div className={styles["toolbar-font-color"]}>
         <Image src={InsertFontIcon} alt={"A icon surrounded with a square."} />
-        <FontAwesomeIcon icon={faAngleDown} style={{ color: "var(--arrow-color)" }} />
+        <FontAwesomeIcon
+          icon={faAngleDown}
+          style={{ color: "var(--arrow-color)" }}
+        />
       </div>
       {/* TODO: palets horizon line */}
       <FontAwesomeIcon icon={faEllipsis} />
