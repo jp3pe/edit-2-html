bc97e07fb657c41a570931cb90518b9f7f4ab819
jp3pe <rlaworhkd430430@gmail.com>
2025-01-12 18:07:33 +0900

    Import icons in Toolbar component to express mock UI

--- app/_components/Toolbar.tsx
+++ app/_components/Toolbar.tsx
@@ -1,12 +1,56 @@
+import Image from "next/image";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faBold,
+  faCircleQuestion,
+  faCode,
+  faEllipsis,
+  faItalic,
+  faLink,
+  faSquareRootVariable,
+  faStrikethrough,
+  faUnderline,
+} from "@fortawesome/free-solid-svg-icons";
+import InsertFontIcon from "@/public/icons/font-color-icon.svg";
 
 interface ToolbarProps {}
 
 export default function Toolbar({}: ToolbarProps) {
   return (
     <div>
-      <FontAwesomeIcon icon={faThumbsUp} className="fa-fw" />
+      <div>
+        <a>
+          {/* TODO: 물음표 기호의 배경과 아이콘을 반전하는 방법 찾기 */}
+          <FontAwesomeIcon
+            icon={faCircleQuestion}
+            style={{ color: "var(--arrow-color)" }}
+          />
+          <p>Info</p>
+        </a>
+      </div>
+      {/* TODO: palets horizon line */}
+      <div>
+        <p>Text</p>
+        <FontAwesomeIcon icon={faAngleDown} style={{ color: "var(--arrow-color)" }} />
+      </div>
+      <FontAwesomeIcon icon={faBold} />
+      <FontAwesomeIcon icon={faItalic} />
+      <FontAwesomeIcon icon={faUnderline} />
+      <FontAwesomeIcon icon={faStrikethrough} />
+      <FontAwesomeIcon icon={faCode} />
+      <FontAwesomeIcon icon={faSquareRootVariable} />
+      <div>
+        <FontAwesomeIcon icon={faLink} />
+        <FontAwesomeIcon icon={faAngleDown} style={{ color: "var(--arrow-color)" }} />
+      </div>
+      <div>
+        <Image src={InsertFontIcon} alt={"A icon surrounded with a square."} />
+        <FontAwesomeIcon icon={faAngleDown} style={{ color: "var(--arrow-color)" }} />
+      </div>
+      {/* TODO: palets horizon line */}
+      <FontAwesomeIcon icon={faEllipsis} />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>{children}</body>
     </html>
   );
