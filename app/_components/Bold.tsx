f7d085ddce4ed7030a0a83a2f0a8e2f36113e3cb
jp3pe <rlaworhkd430430@gmail.com>
2025-01-15 22:36:08 +0900

    Remove <p> tag for the components used to surround them to make block element

--- app/_components/Bold.tsx
+++ app/_components/Bold.tsx
@@ -1,10 +1,5 @@
 interface BoldProps extends TextProps {}
 
 export default function Bold({ innerText }: BoldProps) {
-  return (
-    // TODO: When the component called inside <p> tag in the editor, Remove <p></p> dynamically.
-    <p>
-      <b>{innerText}</b>
-    </p>
-  );
+  return <b>{innerText}</b>;
 }
--- app/_components/Strikethrough.tsx
+++ app/_components/Strikethrough.tsx
@@ -1,10 +1,5 @@
 interface StrikethroughProps extends TextProps {}
 
 export default function Strikethrough({ innerText }: StrikethroughProps) {
-  return (
-    // TODO: When the component called inside <p> tag in the editor, Remove <p></p> dynamically.
-    <p>
-      <s>{innerText}</s>
-    </p>
-  );
+  return <s>{innerText}</s>;
 }
