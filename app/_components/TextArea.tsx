b50b42e61b592303549a67e068523404f8710ce3
jp3pe <rlaworhkd430430@gmail.com>
2025-01-15 22:39:51 +0900

    Add top and bottom margin around the TextArea component

--- app/_components/TextArea.tsx
+++ app/_components/TextArea.tsx
@@ -1,9 +1,11 @@
 import { JSX, ReactElement } from "react";
 
+import styles from "@/app/styles.module.css";
+
 interface TextAreaProps {
   textComponent: ReactElement<JSX.Element>;
 }
 
 export default function TextArea({ textComponent }: TextAreaProps) {
-  return <div>{textComponent}</div>;
+  return <div className={styles["text-area"]}>{textComponent}</div>;
 }
