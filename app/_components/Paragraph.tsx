d07cf4b54e61005751e9897c7d5029d1415b26eb
jp3pe <rlaworhkd430430@gmail.com>
2025-02-05 21:59:46 +0900

    Refactor Paragraph component to simplify content handling and remove unused contentEditable hooks

--- app/_components/Paragraph.tsx
+++ app/_components/Paragraph.tsx
@@ -1,17 +1,9 @@
-import { useContentEditable } from "../_lib/useContentEditable";
-
 interface ParagraphProps extends TextProps {}
 
 export default function Paragraph({ innerText = "\u200B" }: ParagraphProps) {
-  const contentEdit = useContentEditable(innerText);
-
   return (
-    <p
-      contentEditable={true}
-      suppressContentEditableWarning={true}
-      onInput={contentEdit.onInput}
-    >
-      {contentEdit.content}
+    <p contentEditable={true} suppressContentEditableWarning={true}>
+      {innerText}
     </p>
   );
 }
