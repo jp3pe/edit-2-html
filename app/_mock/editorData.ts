2029a119bf2d74ce50529fe7191bcc59ff4bee88
jp3pe <rlaworhkd430430@gmail.com>
2025-02-08 15:24:50 +0900

    Add id key-value for the mock data

--- app/_mock/editorData.ts
+++ app/_mock/editorData.ts
@@ -7,39 +7,45 @@ type AllowedComponentName =
   | "Title";
 
 export interface EditableDynamicComponent {
+  id: number;
   componentName: AllowedComponentName;
   innerText?: string;
 }
 
-export const titleData = [
-  {
-    componentName: "Title",
-    innerText: "Title",
-  },
-];
+export const titleData = {
+  id: 1,
+  componentName: "Title",
+  innerText: "Title",
+};
 
 export const contentData = [
   {
+    id: 1,
     componentName: "Paragraph",
     innerText: "샘플 텍스트입니다.",
   },
   {
+    id: 2,
     componentName: "Bold",
     innerText: "굵게 적용된 샘플 텍스트입니다.",
   },
   {
+    id: 3,
     componentName: "Underline",
     innerText: "밑줄이 적용된 샘플 텍스트입니다.",
   },
   {
+    id: 4,
     componentName: "Strikethrough",
     innerText: "취소선이 적용된 샘플 텍스트입니다.",
   },
   {
+    id: 5,
     componentName: "Italic",
     innerText: "기울림이 적용된 샘플 텍스트입니다.",
   },
   {
+    id: 6,
     componentName: "Paragraph",
     innerText: "샘플 텍스트입니다.",
   },
